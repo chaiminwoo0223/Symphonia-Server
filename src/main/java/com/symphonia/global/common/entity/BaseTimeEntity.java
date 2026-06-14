be558a7a265d89d9429c523b788797d28a396516
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-06-15 01:06:18 +0900

    [#3] feat: BaseTimeEntity, Member, Role, SocialProvider 구현

---
+++ src/main/java/com/symphonia/global/common/entity/BaseTimeEntity.java
@@ -0,0 +1,31 @@
+package com.symphonia.global.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder // 부모 클래스(BaseTimeEntity) 필드까지 빌더에 포함
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass // 자식 엔티티 테이블에 BaseTimeEntity 필드를 컬럼으로 매핑
+@EntityListeners(AuditingEntityListener.class) // @CreatedDate, @LastModifiedDate 자동 주입을 위한 Auditing 리스너 등록
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+}
