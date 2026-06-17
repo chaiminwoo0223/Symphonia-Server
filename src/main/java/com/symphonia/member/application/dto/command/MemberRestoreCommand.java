92ff9d94513e40197431e136fc8882a2ade50ed2
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-06-17 17:10:31 +0900

    [#3] test: MemberRestoreCommandFixture 추가 및 restore 단위 테스트 세분화
    * test: MemberRestoreCommandFixture 추가
    * test: MemberCommandServiceTest에 RestoreByGoogle, RestoreByApple 단위 테스트 추가

--- src/main/java/com/symphonia/member/application/dto/command/MemberRestoreCommand.java
+++ src/main/java/com/symphonia/member/application/dto/command/MemberRestoreCommand.java
@@ -3,7 +3,7 @@ package com.symphonia.member.application.dto.command;
 import com.symphonia.member.domain.entity.SocialProvider;
 
 public record MemberRestoreCommand(
-        String socialId,
-        SocialProvider socialProvider
+        SocialProvider socialProvider,
+        String socialId
 ) {
 }
