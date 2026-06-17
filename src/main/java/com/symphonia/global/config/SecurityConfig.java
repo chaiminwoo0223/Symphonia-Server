131f955ba570adf2de7731c02ebec530a99e59aa
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-06-18 01:31:22 +0900

    [#5] feat: RefreshTokenProvider 구현
    * feat: RefreshTokenProperties 추가
    * feat: SecurityConfig에 RefreshTokenProperties 등록
    
    * refactor: TokenProperties → AccessTokenProperties 이름 수정
    
    * chore: application.yaml에서 jwt.secret, jwt.access-expiration-time → access-token.secret, access-token.expiration-time 변경
    * chore: application.yaml에서 jwt.refresh-expiration-time → refresh-token.expiration-time 변경

--- src/main/java/com/symphonia/global/config/SecurityConfig.java
+++ src/main/java/com/symphonia/global/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.symphonia.global.config;
 
-import com.symphonia.global.config.properties.TokenProperties;
+import com.symphonia.global.config.properties.AccessTokenProperties;
+import com.symphonia.global.config.properties.RefreshTokenProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
 @Configuration
-@EnableConfigurationProperties(TokenProperties.class)
+@EnableConfigurationProperties({AccessTokenProperties.class, RefreshTokenProperties.class})
 public class SecurityConfig {
 }
