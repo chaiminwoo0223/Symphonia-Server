1ac14844d342102361f26b58ea1c8904259f6de6
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-06-19 00:39:42 +0900

    [#5] feat: CustomAuthenticationEntryPoint, CustomAccessDeniedHandler, ErrorResponseWriter 구현

---
+++ src/main/java/com/symphonia/global/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,28 @@
+package com.symphonia.global.security.handler;
+
+import com.symphonia.auth.domain.error.AuthErrorCode;
+import com.symphonia.global.security.writer.ErrorResponseWriter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    private final ErrorResponseWriter errorResponseWriter;
+
+    @Override
+    public void handle(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull AccessDeniedException accessDeniedException
+    ) throws IOException {
+        errorResponseWriter.send(response, AuthErrorCode.ACCESS_DENIED);
+    }
+}
