9ef9d31f5afe81cb5fda8f9c7138c03203fb2d9e
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-06-27 23:42:03 +0900

    [#13] chore: KakaoOAuthProperties 및 OAuthConfig 추가

---
+++ src/main/java/com/symphonia/auth/infrastructure/oauth/config/properties/KakaoOAuthProperties.java
@@ -0,0 +1,13 @@
+package com.symphonia.auth.infrastructure.oauth.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth.kakao")
+public record KakaoOAuthProperties(
+        String clientId,
+        String clientSecret,
+        String redirectUri,
+        String tokenUri,
+        String userInfoUri
+) {
+}
