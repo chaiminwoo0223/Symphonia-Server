df03f42cfb43bee8603db546c50fc01edcea6275
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-06-30 18:45:00 +0900

    [#14] refactor: MemberErrorCode에서 MEMBER_ALREADY_WITHDRAWN, MemberPolicy에서 validateNotWithdrawn 제거

--- src/main/java/com/symphonia/member/domain/policy/MemberPolicy.java
+++ src/main/java/com/symphonia/member/domain/policy/MemberPolicy.java
@@ -7,12 +7,6 @@ import com.symphonia.member.domain.error.MemberErrorCode;
 public enum MemberPolicy {
     ;
 
-    public static void validateNotWithdrawn(Member member) {
-        if (member.isDeleted()) {
-            throw BusinessException.from(MemberErrorCode.MEMBER_ALREADY_WITHDRAWN);
-        }
-    }
-
     public static void validateNotDuplicated(boolean exists) {
         if (exists) {
             throw BusinessException.from(MemberErrorCode.MEMBER_ALREADY_EXISTS);
