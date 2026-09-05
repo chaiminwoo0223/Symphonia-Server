03b9aacb14e4748198b3da9bca3945dae56d74ba
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-09-06 00:03:25 +0900

    [#20] refactor: 와일드카드 import를 개별 import로 변경 * refactor: Member.java jakarta.persistence.* 개별 import로 변경 * refactor: MemberApi.java org.springframework.web.bind.annotation.* 개별 import로 변경 * refactor: RefreshToken.java, BlacklistAccessToken.java lombok.* 개별 import로 변경

--- src/main/java/com/symphonia/member/presentation/MemberApi.java
+++ src/main/java/com/symphonia/member/presentation/MemberApi.java
@@ -8,7 +8,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/v1/members")
 @Tag(name = "Member API", description = "멤버 API")
