e874d4ee4f0fdc4ac264c30e30b48614528ab22c
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-09-06 00:03:50 +0900

    [#20] fix: MemberErrorCode MEMBER_NOT_FOUND 메시지 오타 수정

--- src/main/java/com/symphonia/member/domain/error/MemberErrorCode.java
+++ src/main/java/com/symphonia/member/domain/error/MemberErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements ErrorCode {
 
     // 404
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "요창한 멤버를 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 멤버를 찾을 수 없습니다."),
 
     // 409
     MEMBER_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 회원가입된 멤버입니다."),
