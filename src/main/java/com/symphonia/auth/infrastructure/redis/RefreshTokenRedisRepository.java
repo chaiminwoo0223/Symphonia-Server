338571f9ff878a621910d82e8c9fff5c811bfea5
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-09-05 23:29:02 +0900

    [#21] fix: RefreshToken 삭제가 동작하지 않던 버그 수정

--- src/main/java/com/symphonia/auth/infrastructure/redis/RefreshTokenRedisRepository.java
+++ src/main/java/com/symphonia/auth/infrastructure/redis/RefreshTokenRedisRepository.java
@@ -1,7 +1,8 @@
 package com.symphonia.auth.infrastructure.redis;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, String> {
-    void deleteByMemberId(String memberId);
+    List<RefreshToken> findAllByMemberId(String memberId);
 }
