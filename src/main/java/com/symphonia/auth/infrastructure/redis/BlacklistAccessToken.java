b212ff94f8f73afbd06f74ee77526752479933f3
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-09-06 00:03:25 +0900

    [#20] refactor: 와일드카드 import를 개별 import로 변경 * refactor: Member.java jakarta.persistence.* 개별 import로 변경 * refactor: MemberApi.java org.springframework.web.bind.annotation.* 개별 import로 변경 * refactor: RefreshToken.java, BlacklistAccessToken.java lombok.* 개별 import로 변경

--- src/main/java/com/symphonia/auth/infrastructure/redis/BlacklistAccessToken.java
+++ src/main/java/com/symphonia/auth/infrastructure/redis/BlacklistAccessToken.java
@@ -1,6 +1,10 @@
 package com.symphonia.auth.infrastructure.redis;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
--- src/main/java/com/symphonia/member/domain/entity/Member.java
+++ src/main/java/com/symphonia/member/domain/entity/Member.java
@@ -1,7 +1,13 @@
 package com.symphonia.member.domain.entity;
 
 import com.symphonia.global.common.entity.BaseTimeEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
