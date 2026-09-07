3678518742613a0f410cb51bd979f15e9f9e554e
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-09-07 20:59:28 +0900

    [#35] refactor: BusinessException 서브클래스 계층 도입 * refactor: BusinessException 생성자를 protected로 변경 * refactor: 공통 5종 예외(BadRequestException/UnauthorizedException/ForbiddenException/NotFoundException/ConflictException) 추가

---
+++ src/main/java/com/symphonia/common/exception/UnauthorizedException.java
@@ -0,0 +1,10 @@
+package com.symphonia.common.exception;
+
+import com.symphonia.common.exception.error.ErrorCode;
+
+public class UnauthorizedException extends BusinessException {
+
+    public UnauthorizedException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
