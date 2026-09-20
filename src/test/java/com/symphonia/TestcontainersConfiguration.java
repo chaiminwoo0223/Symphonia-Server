dfa3c24027a526637372e1960fde26aed7e9e553
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-09-20 20:24:44 +0900

    [#53] chore: 데이터베이스를 MySQL에서 PostgreSQL(pgvector)로 전환 * chore: build.gradle의 mysql-connector-j와 testcontainers-mysql을 postgresql과 testcontainers-postgresql로 교체 * chore: compose.yaml의 mysql 서비스를 pgvector/pgvector:pg17 이미지의 postgres 서비스로 교체 * chore: TestcontainersConfiguration의 MySQLContainer를 PostgreSQLContainer로 교체

--- src/test/java/com/symphonia/TestcontainersConfiguration.java
+++ src/test/java/com/symphonia/TestcontainersConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.mysql.MySQLContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -12,8 +12,10 @@ class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
-    MySQLContainer mysqlContainer() {
-        return new MySQLContainer(DockerImageName.parse("mysql:latest"));
+    PostgreSQLContainer postgresContainer() {
+        return new PostgreSQLContainer(
+                DockerImageName.parse("pgvector/pgvector:pg17")
+                        .asCompatibleSubstituteFor("postgres"));
     }
 
     @Bean
